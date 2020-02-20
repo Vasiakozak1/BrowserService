37b664a01b3174e49808a07be63f8b6fc045f979
Vasyl Dubiuk <junglehunter2707@gmail.com>
2020-02-20 21:58:23 +0200

    Added a few action methods

--- BrowserService.js
+++ BrowserService.js
@@ -2,13 +2,19 @@ const puppeteer = require('puppeteer-extra');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 puppeteer.use(StealthPlugin());
 
-export class BrowserService {
+module.exports = class BrowserService {
     constructor() {
         this.Browsers = new Map();
     }
 
-    CreateNew(pathToExtension) {
-        let browser = puppeteer.launch({
+    async getActiveBrowserPageAsync(browser) { // There might be better way of getting current tab.
+        let pages = await browser.pages();
+        return pages[pages.length - 1];
+    }
+
+    async CreateNewAsync(pathToExtension) {
+        console.log("path:" + pathToExtension);
+        let browser = await puppeteer.launch({
             headless: false,
             args: [
                 '-noframemerging',
@@ -20,8 +26,31 @@ export class BrowserService {
         const page = await browser.newPage();
         let browserGuid = this.generateBrowserGuid();
         this.Browsers.set(browserGuid, browser);
-
         return browserGuid;
+    }
+
+    async CloseBrowserAsync(browserGuid) {
+        let browser = this.Browsers.get(browserGuid);
+        await browser.close();
+        this.Browsers.delete(browserGuid);
+    }
+
+    async RedirectToAsync(browserGuid, url) {
+        let browser = this.Browsers.get(browserGuid);
+        let currentPage = await getActiveBrowserPageAsync(browser);
+        await currentPage.goto(url);
+    }
+
+    async InjectJSCodeAsync(browserGuid, jsCode) {
+        let browser = this.Browsers.get(browserGuid);
+        let page = await getActiveBrowserPageAsync(browser);
+        await page.evaluateHandle(jsCode);
+    }
+
+    async InjectJSCodeWithResultAsync(browserGuid, jsCode) {
+        let browser = this.Browsers.get(browserGuid);
+        let page = await getActiveBrowserPageAsync(browser);
+        return await page.evaluateHandle(jsCode);
     }
 
     generateBrowserGuid(){
@@ -30,4 +59,6 @@ export class BrowserService {
             return v.toString(16);
           });
     }
+
+    
 }
--- index.js
+++ index.js
@@ -1,15 +1,44 @@
 const grpc = require('grpc')
-const notesProto = grpc.load('chromiumManipulatorService.proto');
-import { BrowserService } from "./BrowserService";
-
+const chromiumServiceProto = grpc.load('chromiumManipulatorService.proto');
+const BrowserService = require('./BrowserService');
 const browserService = new BrowserService();
-const server = new grpc.Server(notesProto.ChromiumManipulatorService.service, {
-    startChromiumSession: (call, callback) => {
-        console.log(call);
-        //let newBrowserGuid = browserService.CreateNew()
-        callback(null, {browserSessionGuid: 'It seems to be ok'});
-    }
+const server = new grpc.Server();
 
+server.addService(chromiumServiceProto.ChromiumManipulatorService.service, {
+    startChromiumSession: async (call, callback) => {
+        let guid = await browserService.CreateNewAsync(call.request.extensionPath);
+        callback(null, {browserSessionGuid: guid});
+    },
+    endChromiumSession: async (call, callback) => {
+        await browserService.CloseBrowserAsync(call.request.browserSessionGuid);        
+        callback(null, {success: true});
+    },
+    redirectTo: async (call, callback) => {
+        let browserGuid = call.request.browserGuid;
+        let redirectUrl = call.request.url;
+        await browserService.RedirectToAsync(browserGuid, redirectUrl);
+        callback(null, {success: true});
+    },
+    refreshPage: (call, callback) => {
+        callback(null, null);
+    },
+    getCurrentUrl: (call, callback) => {
+        console.log(call);
+        callback(null, null);
+    },
+    injectJs: async (call, callback) => {
+        let browserGuid = call.request.browserGuid;
+        let jsCode = call.request.jsCode;
+        await browserService.InjectJSCodeAsync(browserGuid, jsCode);
+        callback(null, {success: true});
+    },
+    injectJsWithResult: async (call, callback) => {
+        let browserGuid = call.request.browserGuid;
+        let jsCode = call.request.jsCode;
+        let result = await browserService.InjectJSCodeWithResultAsync(browserGuid, jsCode);
+        callback(null, {success: true, result});
+    }
 });
-server.bind('127.0.0.1:5011', grpc.ServerCredentials.createInsecure());
+
+server.bind('127.0.0.1:50051', grpc.ServerCredentials.createInsecure());
 server.start();
